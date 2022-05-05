784b73f6f5ae1ecc892ef8b97d6fc526f5461877
Anton <sychiov2015@yandex.ru>
2022-05-05 22:38:02 +0500

    transition and colorful

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,46 +1,58 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../../resources/logo.svg';
-import burgerMenu from '../../resources/burger-menu.svg';
+import burgerMenuOpen from '../../resources/burger-menu.svg';
+import burgerMenuClose from '../../resources/closeButton.svg';
 import './appHeader.css';
 
 const AppHeader = () => {
 
     const openAndCloseBurger = () => {
-        const header = document.querySelector('.header');
-        header.classList.toggle('burger-header');
-        header.style.zIndex = '777'
-        const menu = document.querySelector('.menu__list');
-        
-       if (menu.style.display === 'block'){
-           menu.style.display = 'none'
-       } else {
-           menu.style.display = 'block'
-       }
+        if (window.screen.width <= 767){
+            const header = document.querySelector('.header');
+            header.classList.toggle('burger-header');
+            header.style.zIndex = '777'
+            const menu = document.querySelector('.menu__list');
+            menu.style.transform = 'translateY(-400px)';
+            setTimeout(() => {
+                menu.style.transform = 'translateY(0px)'
+            }, 0)
+            const burgerOpenButton = document.querySelector('.burger__open');
+            const burgerCloseButton = document.querySelector('.burger__close');
+            burgerOpenButton.style.display = burgerOpenButton.style.display === 'none' ? 'block' : 'none';
+            burgerCloseButton.style.display = burgerCloseButton.style.display === 'none' ? 'block' : 'none';
+
+            if (menu.style.display === 'block'){
+                menu.style.display = 'none'
+            } else {
+                menu.style.display = 'block'
+            }
+        }
     }
     return(
         <header className="header">
             <div className="header__container">
                 <a href="#/" className="header__logo">
-                    <img src={logo} alt="logo" />
+                    <img src={logo} alt="logo"/>
                 </a>
                 <div className="header__timer">Тут должен быть таймер</div>
                 <div className="header__menu menu">
                     <div onClick={openAndCloseBurger} className="menu__button">
-                        <img src={burgerMenu} alt="" />
+                        <img src={burgerMenuOpen} alt="burgerOpen" className="burger__open"/>
+                        <img src={burgerMenuClose} alt="burgerOpen" className="burger__close" style={{display: 'none'}}/>
                     </div>
                     <nav className="menu__body">
                         <ul className="menu__list">
                             <li className="menu__item">
-                                <NavLink to="/timetable" className="menu__link" style={({isActive}) => ({'color' : isActive ? 'red' : '-webkit-link'})}>Расписание</NavLink>
+                                <NavLink onClick={openAndCloseBurger} to="/timetable" className="menu__link" style={({isActive}) => ({'color' : isActive ? 'red' : '#fff'})}>Расписание</NavLink>
                             </li>
                             <li className="menu__item">
-                                <NavLink to="/useful" className="menu__link" style={({isActive}) => ({'color' : isActive ? 'red' : '-webkit-link'})}>Чё то полезное</NavLink>
+                                <NavLink onClick={openAndCloseBurger} to="/useful" className="menu__link" style={({isActive}) => ({'color' : isActive ? 'red' : '#fff'})}>Чё то полезное</NavLink>
                             </li>
                             <li className="menu__item">
-                                <NavLink to="/1000-7" className="menu__link" style={({isActive}) => ({'color' : isActive ? 'red' : '-webkit-link'})}>1000-7???</NavLink>
+                                <NavLink onClick={openAndCloseBurger} to="/1000-7" className="menu__link" style={({isActive}) => ({'color' : isActive ? 'red' : '#fff'})}>1000-7???</NavLink>
                             </li>
                             <li className="menu__item">
-                                <NavLink to="/aboutme" className="menu__link" style={({isActive}) => ({'color' : isActive ? 'red' : '-webkit-link', 'marginRight' : '0px'})}>who i am? </NavLink>
+                                <NavLink onClick={openAndCloseBurger} to="/aboutme" className="menu__link" style={({isActive}) => ({'color' : isActive ? 'red' : '#fff', 'marginRight' : '0px'})}>who i am? </NavLink>
                             </li>
                         </ul>
                     </nav>
--- src/components/pages/inProcess/InProcessPage.js
+++ src/components/pages/inProcess/InProcessPage.js
@@ -2,7 +2,7 @@ import progress from '../../../resources/progress.png'
 const InProcessPage = () => {
     return (
         <div style={{'textAlign': 'center'}}>
-            <img src={progress} alt="progress" style={{'marginLeft': '43%'}}/>
+            <img src={progress} alt="progress" style={{'margin': '0 auto'}}/>
             <h1 style={{'fontSize': '50px'}}>В процессе хуле</h1>
         </div>
         
