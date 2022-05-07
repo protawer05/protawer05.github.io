d395b05776da0fc37ff30323340cdb866c02f9ee
Anton <sychiov2015@yandex.ru>
2022-05-07 14:29:41 +0500

    #

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../components/appHeader/AppHeader"
 import { InProcessPage, MainPage, TimetablePage } from "../components/pages";
 import './app.css'
@@ -9,8 +9,8 @@ const App = () => {
                 <main>
                     <Routes>
                         <Route path="/" element={<MainPage/>}/>
-                        <Route path="timetable" element={<TimetablePage/>}/>
-                        <Route path="*" element={<InProcessPage/>}/>
+                        <Route path="/timetable" element={<TimetablePage/>}/>
+                        <Route path="/*" element={<InProcessPage/>}/>
                     </Routes>
                     <div className="background-img"/>
                 </main>
